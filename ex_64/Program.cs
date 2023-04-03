d7e3eb97931a287f41b1d5c48d63eb34ecf26e36
Mess <alla-fedia@mail.ru>
2023-04-03 14:37:36 +0300

    first commit

---
+++ ex_64/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbers(n);
+
+void PrintNumbers(int n)
+{
+    if (n == 1)
+    {
+        Console.Write(n);
+        return;
+    }
+
+    Console.Write(n + " ");
+    PrintNumbers(n - 1);
+}
---
+++ ex_66/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = Sum(m, n);
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n} равна {sum}");
+
+int Sum(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    else if (m > n)
+    {
+        return 0;
+    }
+    else
+    {
+        return m + Sum(m + 1, n);
+    }
+}
